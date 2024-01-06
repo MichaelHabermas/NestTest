3753b0e5d9b3fd81d0723434d7dcde90c109e103
habermoose <habermoose@gmail.com>
2024-01-05 22:38:24 -0500

    start form validation

--- src/customer/dto/customer.dto.ts
+++ src/customer/dto/customer.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
 export class CustomerDto {
+  @IsNotEmpty()
   acctNumber: number;
+  @IsNotEmpty()
   name: string;
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+  @IsStrongPassword()
+  @IsNotEmpty()
   password: string;
 }
