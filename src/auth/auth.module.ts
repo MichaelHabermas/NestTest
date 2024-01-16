85af2a4a7dc30c3a51f4c9fc4578674543aab8eb
habermoose <habermoose@gmail.com>
2024-01-16 11:02:30 -0500

    service method framing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import {PrismaService} from "../prisma/prisma.service";
 
 @Module({
+  imports: [],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,5 +3,10 @@ import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService {
+  constructor() {
+  }
 
+  async signUp(dto: AuthDto) {}
+
+  async signIn(dto: AuthDto) {}
 }
