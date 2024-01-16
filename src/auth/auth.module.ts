fa29d1c3b2e1efcc71c78802c1c4c54a1191816a
habermoose <habermoose@gmail.com>
2024-01-16 11:25:56 -0500

    start passport strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,3 +14,4 @@ export class AuthService {
 
   async signIn(dto: AuthDto) {}
 }
+
